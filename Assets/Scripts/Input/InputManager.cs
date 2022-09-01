16a760a888b3cead379e36e237a08129db7a6ce7
Bolrik <62060123+Bolrik@users.noreply.github.com>
2022-09-01 17:35:01 +0200

    Initial Commit

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Input
+{
+    public class InputManager : MonoBehaviour
+    {
+        private GameInput Input { get; set; }
+
+        public InputState Move { get; private set; }
+        public InputState View { get; private set; }
+        public InputButton Action { get; private set; }
+        public InputButton AnyKey { get; private set; }
+
+        InputStateUpdate InputStateUpdate { get; set; }
+
+        private void Awake()
+        {
+            this.Input = new GameInput();
+            this.Input.Enable();
+
+            this.Move = this.Add(this.Input.Player.Move);
+            this.View = this.Add(this.Input.Player.View);
+
+            this.Action = this.AddButton(this.Input.Player.Act);
+            this.AnyKey = this.AddButton(this.Input.Player.KeyPress);
+        }
+
+        private InputState Add(InputAction inputAction)
+        {
+            InputStateUpdate inputStateUpdate = this.InputStateUpdate;
+
+            InputState toReturn = new InputState(inputAction, ref inputStateUpdate);
+
+            this.InputStateUpdate = inputStateUpdate;
+
+            return toReturn;
+        }
+
+        private InputButton AddButton(InputAction inputAction)
+        {
+            InputStateUpdate inputStateUpdate = this.InputStateUpdate;
+
+            InputButton toReturn = new InputButton(inputAction, ref inputStateUpdate);
+
+            this.InputStateUpdate = inputStateUpdate;
+
+            return toReturn;
+        }
+
+        private void Update()
+        {
+            this.InputStateUpdate?.Invoke();
+        }
+    }
+
+    public delegate void InputStateUpdate();
+
+
+    public class InputState
+    {
+        public bool WasPressed { get => this.InputAction.WasPressedThisFrame(); }
+        public bool WasReleased { get => this.InputAction.WasReleasedThisFrame(); }
+        public bool IsPressed { get => this.InputAction.IsPressed(); }
+
+        protected InputAction InputAction { get; private set; }
+
+        public InputState(InputAction input, ref InputStateUpdate action)
+        {
+            this.InputAction = input;
+        }
+
+        public Vector2 GetVector2() => this.InputAction.ReadValue<Vector2>();
+
+        public static implicit operator float(InputState inputState) => inputState.InputAction.ReadValue<float>();
+    }
+
+    public class InputButton : InputState
+    {
+        public float DownTime { get; private set; }
+        public float UpTime { get; private set; }
+
+        public InputButton(InputAction input, ref InputStateUpdate action)
+            : base(input, ref action)
+        {
+            action += this.Update;
+        }
+
+
+        void Update()
+        {
+            if (this.IsPressed)
+            {
+                this.DownTime += Time.deltaTime;
+                this.UpTime = 0;
+            }
+            else
+            {
+                this.DownTime = 0;
+                this.UpTime += Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,93 @@
+using Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody rigidbody;
+        public Rigidbody Rigidbody { get { return rigidbody; } }
+
+        [SerializeField] private InputManager input;
+        public InputManager Input { get { return input; } }
+
+
+        [SerializeField] private float moveSpeed;
+        public float MoveSpeed { get { return moveSpeed; } }
+
+
+
+        [SerializeField] private PlayerViewController viewController;
+        public PlayerViewController ViewController { get { return viewController; } }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector2 input = this.Input.View.GetVector2();
+            this.ViewController.Update(input);
+
+            input = this.Input.Move.GetVector2();
+            this.Move(input);
+        }
+
+        void Move(Vector2 moveInput)
+        {
+            Vector3 move = this.transform.forward * moveInput.y * this.MoveSpeed;
+            Vector3 strafe= this.transform.right * moveInput.x * this.MoveSpeed;
+
+            this.Rigidbody.velocity = Vector3.Lerp(this.Rigidbody.velocity, move + strafe, 1 / .3f * Time.deltaTime);
+        }
+    }
+
+    [System.Serializable]
+    public class PlayerViewController
+    {
+        [Header("References")]
+        [SerializeField] private PlayerController player;
+        public PlayerController Player { get { return player; } }
+
+        [SerializeField] private Transform transform;
+        public Transform Transform { get { return transform; } }
+
+        [SerializeField] private Transform head;
+        public Transform Head { get { return head; } }
+
+        [Header("Settings")]
+        [SerializeField] private float rotationSpeedX;
+        public float RotationSpeedX { get { return rotationSpeedX; } }
+
+        [SerializeField] private float rotationSpeedY;
+        public float RotationSpeedY { get { return rotationSpeedY; } }
+
+        [SerializeField] private Vector2 viewConstraint;
+        public Vector2 ViewConstraint { get { return viewConstraint; } }
+
+        [Header("Info")]
+        [SerializeField] private float x;
+        private float X { get { return x; } set { x = value; } }
+
+        [SerializeField] private float y;
+        private float Y { get { return y; } set { y = value; } }
+
+
+
+
+
+        public void Update(Vector2 viewInput)
+        {
+            Vector3 rotation = this.Transform.transform.localEulerAngles;
+            this.Y += viewInput.x * this.RotationSpeedY * Time.deltaTime;
+            rotation.y = this.Y;
+            this.Transform.transform.localEulerAngles = rotation;
+
+            rotation = this.Head.transform.localEulerAngles;
+            this.X -= viewInput.y * this.RotationSpeedX * Time.deltaTime;
+            this.X = Mathf.Clamp(this.X, this.ViewConstraint.x, this.ViewConstraint.y);
+            rotation.x = this.X;
+            this.Head.transform.localEulerAngles = rotation;
+        }
+    }
+}
